1de4ea1037262f34d53daebcecc4018f5577755e
Suhyeon AHN <suahn@e1r8p9.clusters.42paris.fr>
2023-03-10 19:34:30 +0100

    modules

--- tuto/src/app.module.ts
+++ tuto/src/app.module.ts
@@ -1,16 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService],
-})
-
-@Module({
-  controllers: [CatsController],
+  imports: [CatsModule],
 })
 
 export class AppModule {}
--- tuto/src/cats/cats.controller.ts
+++ tuto/src/cats/cats.controller.ts
@@ -1,17 +1,21 @@
 import { Controller, Get, Query, Post, Body, Put, Param, Delete, Header, HttpCode, Redirect } from '@nestjs/common';
-import { CreateCatDto, UpdateCatDto, ListAllEntities } from './dto';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+import { ListAllEntities } from './dto/list-all-entities.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Post()
-  @Header('Cache-Control', 'none')
-  @HttpCode(204)
   create(@Body() createCatDto: CreateCatDto) {
     return 'This action adds a new cat';
   }
 
   @Get()
-  @Redirect('https://nestjs.com', 301)
   findAll(@Query() query: ListAllEntities) {
     return `This action returns all cats (limit: ${query.limit} items)`;
   }
